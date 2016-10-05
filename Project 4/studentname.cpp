93d2127cd35b24ab6e4a31ecebbff256eb95c188
Inteligirl <mischieval@gmail.com>
2016-10-05 06:13:52 -0400

    finished submission

--- Project 4/studentname.cpp
+++ Project 4/studentname.cpp
@@ -17,43 +17,47 @@ int main()
 	string lastStudent;
 	int counter = 0;
 	ifstream inputFile;
+	string stars;   //output formating
 
-	string stars; //output formating
-
+	
 	// Set the star output formatting
-		stars.assign(75, '*');
+	stars.assign(75, '*');
 
 	//Explain fuction of program to user
 	cout << endl << stars << endl;
 	cout << "\nThis program will input student names from the file lineup.txt." << endl;
-	cout << "The student names will be sorted alphabeticaly." << endl;
+	cout << "The student names will be compared alphabeticaly and stored." << endl;
 	cout << "The name of the first student and the last student will then be displayed." << endl << endl;
 	cout << stars << endl << endl;
 
+	//Open text file
 	inputFile.open("lineup.txt");
 
-	if (!inputFile)			//verify file requested did open
+	//verify file requested did open
+	if (!inputFile)			
 	{
 		cout << "Input file did not open." << endl;
 		return 10;
 	}
 
-	while (inputFile >> student)
+	//Extra comments so I can walk myself through what the lines are doing.
+	while (inputFile >> student)  //while the inputFile returns True store read data to variable student
 	{
-		if (counter == 0)
+		if (counter == 0) //The counter will never be 0 except it's first time through. This is an initialization
+			              //to setup the first and last values to compare the remaining students to.
 		{
-			firstStudent = student;
-			lastStudent = student;
-			counter++;
+			firstStudent = student; //it's assigning two at a time. value 1 from file
+			lastStudent = student;  //value 2 from file
+			counter++;              //add 1 to counter
 		}
 
-		else
+		else          //since the counter is now 1 this else statement will execute until the End of File
 		{
-
-			if (student < firstStudent)
-				firstStudent = student;
-			else if (student > lastStudent)
-				lastStudent = student;
+			          //this is where the comparison takes place
+			if (student < firstStudent)  //easier to think of data as a number. If number read is less that firstStudent
+				firstStudent = student;  //then assign that number as the new firstNumber
+			else if (student > lastStudent) //else if larger than the number stored as lastStudent
+				lastStudent = student;      //then assign that number as the new lastStudent
 		}
 	}
 
